c280813ec6080ba2240c9b7a667e718db4d48558
Gabriel Sokol <gsokol4@gmail.com>
2021-01-01 01:25:41 -0600

    worked on making the website responisve for devices under 400px

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ class handleMenuClass {
     constructor(){
     this.state = {displayNav: false}
     this.links = document.getElementsByClassName('container link nav')
+    this.nav = document.getElementsByTagName('nav')
     }
 
     handleMobleMenuClick(){
         this.state.displayNav = !this.state.displayNav
-        
+        console.log(this.nav)
         if (this.state.displayNav === true){
             for (let i = 0; i < this.links.length; i++){
                 this.links[i].style.display = 'none'
+                this.nav[0].style.backgroundColor = 'transparent'
             }
         }else{
             for (let i = 0; i < this.links.length; i++){
                 this.links[i].style.display = 'initial'
+                this.nav[0].style.backgroundColor = '#2F4454'
             }
         }
     
