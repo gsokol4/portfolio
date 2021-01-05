f7eda4fa3d7eb5587c7060e85dbd3c3c122c6eb0
Gabriel Sokol <gsokol4@gmail.com>
2021-01-04 23:49:59 -0600

    worked on adding a google form to the website and making it responsive

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 'use strict'
 
-class handleMenuClass {
-    constructor(){
-    this.state = {displayNav: false}
+class HandleMenuClass {
+  constructor () {
+    this.state = { displayNav: false }
     this.links = document.getElementsByClassName('container link nav')
     this.nav = document.getElementsByTagName('nav')
-    }
+  }
 
-    handleMobleMenuClick(){
-        this.state.displayNav = !this.state.displayNav
-        console.log(this.nav)
-        if (this.state.displayNav === true){
-            for (let i = 0; i < this.links.length; i++){
-                this.links[i].style.display = 'none'
-                this.nav[0].style.backgroundColor = 'transparent'
-            }
-        }else{
-            for (let i = 0; i < this.links.length; i++){
-                this.links[i].style.display = 'initial'
-                this.nav[0].style.backgroundColor = '#2F4454'
-            }
-        }
-    
+  handleMobleMenuClick () {
+    this.state.displayNav = !this.state.displayNav
+    if (this.state.displayNav === true) {
+      for (let i = 0; i < this.links.length; i++) {
+        this.links[i].style.display = 'none'
+        this.nav[0].style.backgroundColor = 'transparent'
+      }
+    } else {
+      for (let i = 0; i < this.links.length; i++) {
+        this.links[i].style.display = 'initial'
+        this.nav[0].style.backgroundColor = '#2F4454'
+      }
     }
+  }
 }
-let handleMenu = new handleMenuClass
+const handleMenu = new HandleMenuClass()
