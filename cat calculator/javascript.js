680cca769a2939a6ebd3343e71df5106e6da5575
Gabriel Sokol <gsokol4@gmail.com>
2021-01-12 02:31:23 -0600

    edited the margins on the main page aswell as added quotes marks to the calculator.

--- cat calculator/javascript.js
+++ cat calculator/javascript.js
@@ -219,17 +219,17 @@ const catImages = ['gifs/plusheen.gif', 'plusheenAdd.png', 'plusheenButt.png', '
   'plusheenMeh.png', 'plusheenQuestion.png', 'plusheenScooter.png', 'plusheenSurprise.png',
   'plusheenwink.png']
 
-const catQuotes = ['In ancient times cats were worshipped as gods; they have not forgotten this. - Terry Pratchett',
-  'I had been told that the training procedure with cats was difficult. It is not. Mine had me trained in two days. - Bill Dana',
-  'Cats are inquisitive, but hate to admit it. - Mason Cooley',
-  'As anyone who has ever been around a cat for any length of time well knows, cats have enormous patience with the limitations of the humankind. - Cleveland Amory',
-  'I have studied many philosophers and many cats. The wisdom of cats is infinitely superior. - Hippolyte Taine',
-  'There are two means of refuge from the miseries of life: music and cats. - Albert Schweitzer',
-  'Owners of dogs will have noticed that, if you provide them with food and water and shelter and affection, they will think you are God. Whereas owners of cats are compelled to realize that,if you provide them with food and water and affection, they draw the conclusion that they are God. - Christopher Hitchens',
-  'A happy arrangement: many people prefer cats to other people, and many cats prefer people to other cats. - Mason Cooley',
-  'It is impossible for a lover of cats to banish these alert, gentle, and discriminating friends,  who give us just enough of their regard and complaisance to make us hunger for more. - Agnes Repplier',
-  'How we behave toward cats here below determines our status in heaven. - Robert A. Heinlein',
-  'I used to love dogs until I discovered cats. - Nafisa Joseph'
+const catQuotes = ['"In ancient times cats were worshipped as gods; they have not forgotten this." - Terry Pratchett',
+  '"I had been told that the training procedure with cats was difficult. It is not. Mine had me trained in two days." - Bill Dana',
+  '"Cats are inquisitive, but hate to admit it." - Mason Cooley',
+  '"As anyone who has ever been around a cat for any length of time well knows, cats have enormous patience with the limitations of the humankind." - Cleveland Amory',
+  '"I have studied many philosophers and many cats. The wisdom of cats is infinitely superior." - Hippolyte Taine',
+  '"There are two means of refuge from the miseries of life: music and cats." - Albert Schweitzer',
+  '"Owners of dogs will have noticed that, if you provide them with food and water and shelter and affection, they will think you are God. Whereas owners of cats are compelled to realize that,if you provide them with food and water and affection, they draw the conclusion that they are God." - Christopher Hitchens',
+  '"A happy arrangement: many people prefer cats to other people, and many cats prefer people to other cats." - Mason Cooley',
+  '"It is impossible for a lover of cats to banish these alert, gentle, and discriminating friends,  who give us just enough of their regard and complaisance to make us hunger for more." - Agnes Repplier',
+  '"How we behave toward cats here below determines our status in heaven." - Robert A. Heinlein',
+  '"I used to love dogs until I discovered cats." - Nafisa Joseph'
 ]
 
 /* cat quote generator (=♡ ᆺ ♡=)
